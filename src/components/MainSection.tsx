e2e9f40fdcaa52f39dfa8802eba1fde5daeb69f5
izuna69 <ddong17593@gmail.com>
2026-08-27 17:04:53 +0900

    1

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -45,7 +45,7 @@ const MainSection = () => {
                                 className="rounded-md bg-gray-700 px-3.5 py-2.5 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-gray-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                             >
                                 {' '}
-                                アプリのダウンロードはこちら
+                                주찬아 여기 눌러봐
                             </a>
                             <a href="/ErrorPage" className="text-sm/6 font-semibold text-white hover:text-gray-100">
                                 詳細
